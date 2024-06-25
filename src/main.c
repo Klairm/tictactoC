76ce074e738d188bc5d46b611a77d78b1f4a2c69
ivang <ivangardis@proton.me>
2024-06-25 23:35:09 +0200

    feat: handle user input

--- src/main.c
+++ src/main.c
@@ -1,30 +1,66 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 
-const int CROSS = 1;
-const int CIRCLE = 2;
+#define CROSS 1
+#define CIRCLE 2
 
 int gameTable[3][3];
+int token = CIRCLE;
 
 int
 main(){
-
+	bool exit = false;
 	extern int gameTable[3][3] ;
 
 	initTable(gameTable);
 
-	showTable();
+	printf("Enter movement, O starts first.\n0 to exit. \n ");
+	while(!exit){
 
+		if(getUserInput() == 1){
+			exit = true;
+		}else{
+
+		showTable();
+		}
+
+	}
 
 
 }
 
+int
+getUserInput(){
+	extern int gameTable[3][3];
+	int row = 0, col = 0;
+	extern int token;
+
+	printf("Select the position: "); scanf("%d %d",&row,&col);
+	printf("\n");
+	gameTable[row][col] = token ;
+	if(token == CIRCLE ){
+		token = CROSS;
+	}else{
+		token = CIRCLE;
+	}
+	return row == 0;
+}
+
 void
 showTable(){
+	extern int gameTable[3][3];
+
 	for(int row = 0; row < 3; row++){
 		for(int column = 0; column < 3; column++){
-			printf("- ");
+			switch(gameTable[row][column]){
+				case CIRCLE: printf("O "); break;
+				case CROSS: printf("X "); break;
+				default: 
+					printf("- ");
+				break;
+			}
 
 		}
 		printf("\n");
